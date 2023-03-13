0b89dc74741deeb52058ae3ef1876602442130a9
owusuvictorjnr <owusuvictor80@gmail.com>
2023-03-13 19:07:30 +0000

    Create homepage

---
+++ components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './Layout';
+
+const Home = () => {
+  return (
+    <>
+      <div className="min-h-screen">
+        <div className="min-h-screen">Home</div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
